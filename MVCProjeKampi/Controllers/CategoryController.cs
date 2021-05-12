33c86af85ad4d640228d123f383c713928e557e9
husokanus <husokanus@gmail.com>
2021-05-12 22:16:42 +0300

    added missing files

---
+++ MVCProjeKampi/Controllers/CategoryController.cs
@@ -0,0 +1,39 @@
+﻿using BusinessLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCProjeKampi.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        CategoryManager cm = new CategoryManager();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetCategoryList()
+        {
+            var categoryValues = cm.GetAllBL();
+            return View(categoryValues);
+        }
+
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCategory(Category ct)
+        {
+            cm.CategoryAddBL(ct);
+            return RedirectToAction("GetCategoryList");
+        }
+    }
+}
